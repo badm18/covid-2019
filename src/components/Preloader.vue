bb3d424ceb4c64ed2401cb83f2578f34e77abf66
mikhail <shatilovmikhailareal@gmail.com>
2021-12-11 23:17:50 +0300

    Preloader

---
+++ src/components/Preloader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="loading web-view-mode">
+    <div class="container-app-loader" id="start-app-loader">
+      <div class="container-pulse pulse animated"></div>
+      <div class="container-app-logo">
+        <div class="app-logo"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Preloader",
+};
+</script>
