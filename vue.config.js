385a081e271a067ccd7a4edac52fdd58643e3b7e
mikhail <shatilovmikhailareal@gmail.com>
2021-12-25 19:43:28 +0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  publicPath: "/gosusvslugi.cf/covid-cert/verify/",
+  publicPath: "/gosusvslugi.cf/",
   outputDir: path.resolve(__dirname, "./docs"),
 };
